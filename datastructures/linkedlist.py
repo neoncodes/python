30b98e7dd4a01b07b73774f2502b11e433a9ef2b
Charley Wolf <charleywolfofficial@gmail.com>
2022-07-04 10:51:43 -0400

    yo

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -3,70 +3,66 @@ class Node:
         self.value = value
         self.nextNode = nextNode
 
+
 class LinkedList:
-    def __init__(self, head):
-        self.head = Node(head, None)
+    def __init__(self):
+        self.head = None
+        self.size = 0
+        self.tail = None
 
     def append(self, value):
-        temp = self.head
-        while temp.nextNode != None:
-            temp = temp.nextNode
-        temp.nextNode = Node(value, None)
-        return True
+        if not self.head:
+            self.head = Node(value, None)
+            self.tail = self.head
+        else:
+            self.tail.nextNode = Node(value, None)
+            self.tail = self.tail.nextNode
+        self.size += 1
 
     # Returning Functions (print, size)
 
-    def print(self):
+    def __repr__(self):
         temp = self.head
         string = str(temp.value)
-        while temp.nextNode != None:
+        while temp.nextNode:
             temp = temp.nextNode
-            string += ", "+str(temp.value)
+            string += ", " + str(temp.value)
         return string
 
     def size(self):
-        temp = self.head
-        s = 1
-        while temp.nextNode != None:
-            s = s+1
-            temp = temp.nextNode
-        return s
+        return self.size
 
     # Removal Functions
-    
+
     def remove(self, val):
         temp = self.head
-        while temp.nextNode != None:
+        x = 0
+        if not temp:
+            return False
+        if temp.value == val:
+            self.head = self.head.nextNode
+        while temp.nextNode:
             if temp.nextNode.value == val:
                 if temp.nextNode.nextNode:
                     temp.nextNode = temp.nextNode.nextNode
                 else:
                     temp.nextNode = None
                 return True
+            x += 1
             temp = temp.nextNode
-        return False
-
-    def pop(self, ind):
+        self.size -= 1
+        return x
+    def pop(self, y):
         temp = self.head
-        cInd = 0
-        while temp.nextNode != None:
-            if cInd == ind-1:
+        x = 0
+        while temp.nextNode:
+            if x == y - 1:
                 if temp.nextNode.nextNode:
                     temp.nextNode = temp.nextNode.nextNode
                 else:
                     temp.nextNode = None
                 return True
             temp = temp.nextNode
-            cInd = cInd + 1
+            x = x + 1
+        self.size -= 1
         return False
-
-lList = LinkedList("A")
-lList.append("B")
-lList.append("C")
-lList.append("D")
-lList.append("E")
-lList.append("F")
-lList.remove("D")
-lList.pop(4)
-print(lList.size())
-print(lList.print())
